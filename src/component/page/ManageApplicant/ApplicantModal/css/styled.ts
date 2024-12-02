3cb64380c8442b10c9a52f7ad61f507f166a844b
katpaking <katpaking@naver.com>
2024-12-02 16:10:51 +0900

    탈퇴 빼고 스타일 추가

--- src/component/page/ManageApplicant/ApplicantModal/css/styled.ts
+++ src/component/page/ManageApplicant/ApplicantModal/css/styled.ts
@@ -1,23 +1,39 @@
 import styled from "styled-components";
 
-// 스타일 정의 (div 없이)
-export const MyPageStyled = styled.section`
-    padding-top: 20px; /* 페이지 상단에 여백 추가 */
+export const ApplicantModalStyled = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 700px; /* 최대 너비 설정 */
+    height: auto;
+    max-height: 950px; /* 최대 높이를 화면의 80%로 설정 */
+    z-index: 1000;
     display: flex;
-    justify-content: left; /* 페이지 중앙에 정렬 */
-    padding: 0 0px; /* 좌우 여백 */
-    width: 1200px;
-    overflow: auto;
+    flex-direction: column; /* 세로로 정렬 */
+    background: #dcdcdc;
+    justify-content: flex-start; /* 내용 위쪽으로 정렬 */
+    align-items: center;
+    padding: 10px 20px; /* 좌우 여백을 줄임 */
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+    border: 1px solid #ccc;
+    overflow: auto; /* 내용이 넘치면 스크롤 생기게 설정 */
 
-    .selectUserType {
-        padding: 8px;
-        margin-top: 5px;
-        margin-bottom: 5px;
-        border-radius: 5px;
-        border: 1px solid #ccc;
+    .title-container {
+        text-align: center;
+        margin-bottom: 0px; /* 제목과 테이블 사이에 여백을 줄임 */
+        font-size: 1.5em;
+        font-weight: bold;
     }
 
-    .selectBirth {
+    .strong {
+        font-size: 1.5em;
+        font-weight: bold;
+        display: block;
+        margin-bottom: 10px; /* 제목과 내용 사이 여백을 줄임 */
+    }
+
+    .selectUserType {
         padding: 8px;
         margin-top: 5px;
         margin-bottom: 5px;
@@ -28,43 +44,29 @@ export const MyPageStyled = styled.section`
     .table {
         display: flex;
         flex-direction: column;
-        width: 1000px;
+        width: 100%;
         text-align: left;
         border-collapse: collapse;
         margin-bottom: 20px; /* 테이블과 버튼 사이에 여백 추가 */
     }
 
+    th,
     td {
-        width: 900px;
         padding: 10px;
         text-align: left;
         border: 1px solid #ddd;
     }
 
     th {
-        padding: 20px;
-        text-align: center;
-        width: 150px;
         background-color: #dcdcdc;
         vertical-align: middle;
-        position: relative; /* 가상요소 위치를 위한 설정 */
-
-        &.required::after {
-            content: "*";
-            color: #ff0000;
-            position: absolute;
-            margin-left: 5px;
-            top: 50%;
-            transform: translateY(-50%);
-            font-size: 1.2em;
-        }
     }
 
     input[type="text"],
     input[type="password"],
     input[name="userName"] {
         padding: 8px;
-        width: 700px;
+        width: 350px;
         margin-top: 5px;
         margin-bottom: 5px;
         border: 1px solid #ccc;
@@ -95,19 +97,20 @@ export const MyPageStyled = styled.section`
         padding: 10px 22px;
         text-align: center;
         font-size: 16px;
+        margin: 4px 2px;
         cursor: pointer;
         border-radius: 12px;
         box-shadow: 0 4px #999;
         transition: 0.3s;
-    }
 
-    button:hover {
-        background-color: #45a049;
-    }
+        &:hover {
+            background-color: #45a049;
+        }
 
-    button:active {
-        background-color: #3e8e41;
-        box-shadow: 0 2px #666;
-        transform: translateY(2px);
+        &:active {
+            background-color: #3e8e41;
+            box-shadow: 0 2px #666;
+            transform: translateY(2px);
+        }
     }
 `;
